51b687fbcd92af0e5015dae8c0ad366593f83b1f
jisuSong0625 <108786142+jisuSong0625@users.noreply.github.com>
2023-05-25 23:45:37 +0900

    .

---
+++ app/src/main/java/com/sjs/myapplication/Problem1.java
@@ -0,0 +1,14 @@
+package com.sjs.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Problem1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problem1);
+    }
+}
